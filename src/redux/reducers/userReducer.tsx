0d8b9ce3535ffb57e8d8ab5d273dead2dc9e7d18
Shuyuan <kukutou_mikiya@outlook.com>
2022-07-13 22:36:27 -0700

    feat: add login to header

--- src/redux/reducers/userReducer.tsx
+++ src/redux/reducers/userReducer.tsx
@@ -9,7 +9,7 @@ const initialState = {
 };
 
 // eslint-disable-next-line @typescript-eslint/default-param-last
-export default function (state = initialState, action:any) {
+export default function (state = initialState, action: any) {
   switch (action.type) {
     case SET_AUTHENTICATED:
       return {
