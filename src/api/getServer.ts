ff5a1cc74f0e4326811a31ca58de2cfcd8e125f5
bunnyxt <bunnyxt@outlook.com>
2022-07-16 01:25:04 -0700

    feat: load token from local storage `lvz-token`

--- src/api/getServer.ts
+++ src/api/getServer.ts
@@ -3,13 +3,13 @@ import axios from 'axios';
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 const getServer = () => {
-  // const token = localStorage.getItem('token') ?? ''; // TODO: change name
+  const token = localStorage.getItem('lvz-token') ?? '';
   const customedAxios = axios.create({
     baseURL: BACKEND_URL,
     timeout: 10000,
-    // headers: {
-    //   Authorization: token,
-    // },
+    headers: {
+      Authorization: token,
+    },
   });
   return customedAxios;
 };
