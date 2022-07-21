109b9d4f6cc1fcafc2c6d5c8c41a562d64b5a3f1
Shuyuan <kukutou_mikiya@outlook.com>
2022-07-20 19:22:36 -0700

    chore: update denpendency

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,13 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-// ...
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import rootReducer from '../_reducers';
 
-const store = configureStore({
-  reducer: {
+const loggerMiddleware = createLogger();
 
-  },
-});
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+export const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware,
+  ),
+);
