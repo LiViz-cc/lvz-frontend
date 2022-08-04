b45e1c1c06b9434ec5668be23829c25c89e2a8e3
bunnyxt <bunnyxt@outlook.com>
2022-08-03 18:00:19 -0700

    feat: add naive demo

--- src/components/common/ProjectEditor.tsx
+++ src/components/common/ProjectEditor.tsx
@@ -1,14 +1,61 @@
 import React, { FC } from 'react';
+import Split from 'react-split';
+import { Button } from 'antd';
+import * as echarts from 'echarts';
 import './ProjectEditor.scss';
 
 type ProjectEditorProps = {
   id: string
 };
 
-const ProjectEditor: FC<ProjectEditorProps> = ({ id }: ProjectEditorProps) => (
-  <div className="project-editor">
-    {`Project Editor, id: ${id}`}
-  </div>
-);
+const ProjectEditor: FC<ProjectEditorProps> = ({ id }: ProjectEditorProps) => {
+  const renderDemo = () => {
+    const chartPreviewEl = document.getElementById('chart-preview');
+    if (!chartPreviewEl) {
+      return;
+    }
+    const chartPreview = echarts.init(chartPreviewEl);
+    if (!chartPreview) {
+      return;
+    }
+    const chartOption = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line',
+        },
+      ],
+    };
+    chartPreview.setOption(chartOption);
+  };
+
+  return (
+    <div className="project-editor">
+      <div className="header-section">
+        header
+        {' '}
+        <Button onClick={renderDemo}>render demo</Button>
+      </div>
+      <Split className="main-section">
+        <div className="left-section">
+          {`Project Editor, left section, id: ${id}`}
+        </div>
+        <div className="center-section">
+          <div id="chart-preview" style={{ width: 600, height: 400 }} />
+        </div>
+        <div className="right-section">
+          {`Project Editor, right section, id: ${id}`}
+        </div>
+      </Split>
+    </div>
+  );
+};
 
 export default ProjectEditor;
